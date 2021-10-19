8bf49b6e5dd672c6a77e296ad43e365933b6d41e
Maria Rotolo <mariarotolo1994@gmail.com>
2021-10-19 17:56:15 +0200

    modifica javascript

---
+++ app.js
@@ -0,0 +1,43 @@
+/*
+
+let animal = prompt ("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda ");
+
+
+if (animal == 1) {
+    console.log( "chose the dog");
+}else
+if (animal == 2) {
+    console.log( "chose the cat");
+} else 
+if (animal == 3) {
+    console.log( "chose the goldfish");
+} else 
+if (animal == 4) {
+    console.log( "chose the monkey");
+} else 
+if (animal == 5) {
+    console.log ("chose the panda");
+}
+
+*/
+
+let animal = prompt ("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda ");
+
+switch(animal) {
+    case 1:
+   alert( "chose the dog");
+    break
+    case 2:
+    alert("chose the cat");
+    break
+    case 3:
+    alert("chose the goldfish");
+    break
+    case 4:
+    alert("chose the monkey");
+    break
+    case 5:
+    alert("chose the panda");
+    break
+
+}
